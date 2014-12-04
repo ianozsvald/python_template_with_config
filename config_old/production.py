03b5bb790a2dbef8c72cf601662a5063dc475d5a
Ian Ozsvald <ian@ianozsvald.com>
2014-12-04 12:28:31 +0000

    minor

---
+++ config_old/production.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""production configuration (imported by __init__.py)"""
---
+++ config_old/testing.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Testing configuration (imported by __init__.py)"""
